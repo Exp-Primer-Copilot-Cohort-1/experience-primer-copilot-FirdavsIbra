58006fdc86b5058b4a82afbafafea5b6db70bb48
Firdavs <103358030+FirdavsIbra@users.noreply.github.com>
2024-03-20 12:32:29 +0500

    Copilot fifth commit

--- comment.js
+++ comment.js
@@ -6,10 +6,3 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
-
-// 2. Create a route for the home page
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-}
-);
